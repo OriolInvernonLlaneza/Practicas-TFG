5d491fa504da77bc0fb49be297e94429b42593da
OriolInvernonLlaneza <93silf@gmail.com>
2018-01-24 22:51:28 +0100

    Clean followup (d3 is not defined warnings are going to stay) Backup js not necessary (drag renamed to graph // graph deleted) Working wiki code to wiki.js

--- webPracticas/resources/graph.js
+++ webPracticas/resources/graph.js
@@ -1,65 +1,154 @@
-//color scheme
-var colors = d3.scaleOrdinal(d3.schemeCategory10);
+function createGraph(svg, graph) {
 
-//Create svg
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    node,
-    link;
+    var brush;
+    var brushMode = false;
+    var brushing = false;
+    var shiftKey;
+    var simulation;
+    var node;
 
-//Create force layout
-var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function (d) { return d.id; }).distance(100).strength(1))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    // take size from svg
+    var width = +svg.attr("width"),
+        height = +svg.attr("height");
 
-//force functions
-function dragstarted(d) {
-    if (!d3.event.active) {
-        simulation.alphaTarget(0.3).restart()
+    // color scheme
+    var fill = d3.scaleOrdinal(d3.schemeCategory20);
+
+    // remove any previous graphs
+    svg.selectAll(".g-main").remove();
+
+    // create main frame
+    var gMain = svg.append("g")
+        .classed("g-main", true);
+
+    // append white rect to frame
+    var rect = gMain.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "white");
+
+    // append other g to frame    
+    var gDraw = gMain.append("g");
+
+    // force functions
+    function dragstarted(d) {
+        if (!d3.event.active) { simulation.alphaTarget(0.3).restart(); }
+
+        if (!d.selected && !shiftKey) {
+            // if this node isn't selected, then we have to unselect every other node
+            node.classed("selected", function (p) {
+                return p.selected = p.previouslySelected = false;
+            });
+        }
+
+        d3.select(this).classed("selected", function (p) {
+            d.previouslySelected = d.selected;
+            return d.selected = true; // selection
+        });
+
+        node.filter(function (d) { return d.selected; })
+            .each(function (d) { // move the selected
+                d.fx = d.x;
+                d.fy = d.y;
+            });
+
     }
-    d.fx = d.x;
-    d.fy = d.y;
-}
 
-function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-}
-
-function dragended(d) {
-    if (!d3.event.active) {
-        simulation.alphaTarget(0);
+    function dragged(d) {
+        node.filter(function (d) { return d.selected; })
+            .each(function (d) {
+                d.fx += d3.event.dx;
+                d.fy += d3.event.dy;
+            });
     }
-    d.fx = undefined;
-    d.fy = undefined;
-}
 
-//Read from json
-d3.json("resources/jovellanos.json", function (error, graph) {
-    if (error) {
-        throw error;
+    function dragended(d) {
+        if (!d3.event.active) { simulation.alphaTarget(0); }
+        d.fx = null;
+        d.fy = null;
+        node.filter(function (d) { return d.selected; })
+            .each(function (d) {
+                d.fx = null;
+                d.fy = null;
+            });
     }
-    update(graph.links, graph.nodes);
-});
 
-//
-function update(links, nodes) {
-    //create link line, width increases with number of letters
-    link = svg.selectAll(".link")
-        .data(links)
-        .enter()
-        .append("line")
+    // zoom list
+    function zoomed() {
+        gDraw.attr("transform", d3.event.transform); // transforms drawing with zoom event
+    }
+
+    var zoom = d3.zoom() // Creates new zoom behavior (obj and func) https://github.com/d3/d3-zoom
+        .on("zoom", zoomed); // add listener
+
+    gMain.call(zoom);
+
+    // return if no links are found
+    if (!("links" in graph)) {
+        //console.log("No links found");
+        return;
+    }
+
+    //array for nodes
+    var nodes = {};
+    // search nodes in json (graph), add them to array
+    // assign weight
+    for (var i = 0; i < graph.nodes.length; i++) {
+        nodes[i+1] = graph.nodes[i]; // Codacy takes this as unsafe code but it should not be given that there is no user input
+        graph.nodes[i].weight = 1.01; // https://blog.liftsecurity.io/2015/01/14/the-dangers-of-square-bracket-notation/
+    }                                    
+
+    // the brush needs to go before the nodes so that it doesn"t
+    // get called when the mouse is over a node
+    var gBrushHolder = gDraw.append("g");
+    var gBrush = null;
+
+    //add links, stroke = number of letters
+    var link = gDraw.append("g")
         .attr("class", "link")
+        .selectAll("line")
+        .data(graph.links)
+        .enter().append("line")
         .attr("stroke-width", function (d) { return d.value; });
 
-    link.append("title")
-        .text(function (d) { return d.mood; });
+    //add nodes
+    node = gDraw.append("g")
+        .attr("class", "node")
+        .selectAll("circle")
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("r", 10)
+        .attr("fill", function (d) { return d.color; })
+        .call(d3.drag() // drag functions on node
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
 
-    //path obj
-    edgepaths = svg.selectAll(".edgepath")
-        .data(links)
+    // tooltip titles
+    node.append("title")
+        .text(function (d) { return d.name; });
+
+    //node name //Spaghetti si quito esto label no funciona, no debería pasar
+    node.append("text")
+        .attr("dy", -10)
+        .text(function (d) { return d.name; });
+
+    //node label
+    var label = gDraw.append("g")
+        .attr("class", "label")
+        .selectAll("label")
+        .data(graph.nodes)
+        .enter().append("text")
+        .style("pointer-events", "none")
+        .text(function (d) { return d.name; })
+        .style("text-anchor", "middle")
+        .style("fill", "#555")
+        .style("font-family", "Arial")
+        .style("font-size", 12);
+
+    // path under link
+    var edgepaths = gDraw.selectAll(".edgepath")
+        .data(graph.links)
         .enter()
         .append("path")
         .attr("class", "edgepath")
@@ -68,84 +157,154 @@ function update(links, nodes) {
         .attr("stroke-opacity", 0)
         .style("pointer-events", "none");
 
-    //link label obj
-    edgelabels = svg.selectAll(".edgelabel")
-        .data(links)
+    // path label
+    var edgelabels = gDraw.selectAll(".edgelabel")
+        .data(graph.links)
         .enter()
         .append("text")
         .style("pointer-events", "none")
         .attr("class", "edgelabel")
-        .attr("id", function (d, i) { return "edgelabel" + i })
-        .attr("font-size", 15)
-        .attr("fill", "#000");
+        .attr("id", function (d, i) { return "edgelabel" + i; })
+        .style("fill", "#555")
+        .style("font-family", "Arial")
+        .style("font-size", 12);
 
-    //link label text
+    // path label text
     edgelabels.append("textPath")
-        .attr("xlink:href", function (d, i) { return "#edgepath" + i })
+        .attr("xlink:href", function (d, i) { return "#edgepath" + i; })
         .style("text-anchor", "middle")
         .style("pointer-events", "none")
         .attr("startOffset", "50%")
         .text(function (d) { return d.mood });
 
-    //node obj
-    node = svg.selectAll(".node")
-        .data(nodes)
-        .enter()
-        .append("g")
-        .attr("class", "node")
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended)
-        );
+    simulation = d3.forceSimulation() // create and start simulation
+        .force("link", d3.forceLink().id(function (d) { return d.id; }).distance(100).strength(1))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2))
 
-    //node drawing
-    node.append("circle")
-        .attr("r", 10)
-        .style("fill", function (d, i) { return colors(i); });
+    function ticked() {
+        // update node, link, label and path positions
+        // at every step of the simulation
+        link.attr("x1", function (d) { return d.source.x; })
+            .attr("y1", function (d) { return d.source.y; })
+            .attr("x2", function (d) { return d.target.x; })
+            .attr("y2", function (d) { return d.target.y; });
 
-    //node title
-    node.append("title")
-        .text(function (d) { return d.id; });
+        node.attr("cx", function (d) { return d.x; })
+            .attr("cy", function (d) { return d.y; });
 
-    //node name
-    node.append("text")
-        .attr("dy", -10)
-        .text(function (d) { return d.name; });
+        label.attr("x", function (d) { return d.x; })
+            .attr("y", function (d) { return d.y - 10; });
 
+        edgepaths.attr("d", function (d) {
+            return "M " + d.source.x + " " + d.source.y + " L " + d.target.x + " " + d.target.y;
+        });
+
+        edgelabels.attr("transform", function (d) {
+            if (d.target.x < d.source.x) { // rotate text 
+                var bbox = this.getBBox(); // get svg bounding box
+
+                var rx = bbox.x + bbox.width / 2;
+                var ry = bbox.y + bbox.height / 2;
+                return "rotate(180 " + rx + " " + ry + ")";
+            }
+            else {
+                return "rotate(0)";
+            }
+        });
+    }
+    
     simulation
-        .nodes(nodes)
+        .nodes(graph.nodes) // add nodes to simulation
         .on("tick", ticked);
 
-    simulation.force("link")
-        .links(links);
-}
+    simulation.force("link") // add links to simulation
+        .links(graph.links);
 
-//tick behaviour
-function ticked() {
-    link
-        .attr("x1", function (d) { return d.source.x; })
-        .attr("y1", function (d) { return d.source.y; })
-        .attr("x2", function (d) { return d.target.x; })
-        .attr("y2", function (d) { return d.target.y; });
-
-    node
-        .attr("transform", function (d) { return "translate(" + d.x + ", " + d.y + ")"; });
-
-    edgepaths.attr("d", function (d) {
-        return "M " + d.source.x + " " + d.source.y + " L " + d.target.x + " " + d.target.y;
+    //click on frame -> delete selection
+    rect.on("click", () => {
+        node.each(function (d) {
+            d.selected = false;
+            d.previouslySelected = false;
+        });
+        node.classed("selected", false); // Remove CSS class from the selection
     });
 
-    edgelabels.attr("transform", function (d) {
-        if (d.target.x < d.source.x) {
-            var bbox = this.getBBox();
+    function brushstarted() {
+        // keep track of whether we're actively brushing so that we
+        // don"t remove the brush on keyup in the middle of a selection
+        brushing = true;
 
-            rx = bbox.x + bbox.width / 2;
-            ry = bbox.y + bbox.height / 2;
-            return "rotate(180 " + rx + " " + ry + ")";
+        node.each(function (d) {
+            d.previouslySelected = shiftKey && d.selected;
+        });
+    }
+
+    function brushed() {
+        if (!d3.event.sourceEvent) { return; }
+        if (!d3.event.selection) { return; }
+
+        var extent = d3.event.selection; // brushable area
+
+        node.classed("selected", function (d) { // node class selected if inside extent or prev selected.
+            return d.selected = d.previouslySelected ^ // XOR
+                (extent[0][0] <= d.x && d.x < extent[1][0]
+                    && extent[0][1] <= d.y && d.y < extent[1][1]);
+        });
+    }
+
+    function brushended() {
+        if (!d3.event.sourceEvent) { return; }
+        if (!d3.event.selection) { return; }
+        if (!gBrush) { return; }
+
+        gBrush.call(brush.move, null); // clear brush selection
+
+        if (!brushMode) {
+            // the shift key has been release before we ended our brushing
+            gBrush.remove();
+            gBrush = null;
         }
-        else {
-            return "rotate(0)";
+
+        brushing = false;
+    }
+
+    brush = d3.brush() // brush functions
+        .on("start", brushstarted)
+        .on("brush", brushed)
+        .on("end", brushended);
+
+    function keydown() {
+        shiftKey = d3.event.shiftKey; //push shift
+
+        if (shiftKey) { //if true shift was pressed
+            // if brush on, do nothing
+            if (gBrush) { return; }
+
+            brushMode = true; // modo brush (mouse as cross)
+            if (!gBrush) {
+                gBrush = gBrushHolder.append("g");
+                gBrush.call(brush);
+            }
         }
-    });
+    }
+
+    function keyup() {
+        shiftKey = false;
+        brushMode = false;
+
+        if (!gBrush) { return; }
+
+        if (!brushing) {
+            // only remove the brush if we"re not actively brushing
+            // otherwise it"ll be removed when the brushing ends
+            gBrush.remove();
+            gBrush = null;
+        }
+    }
+
+    d3.select("body").on("keydown", keydown); // event handler that starts the brushing
+    d3.select("body").on("keyup", keyup); // event handler that ends the brushing
+
+    return graph;
 }
---
+++ webPracticas/resources/wiki.js
@@ -0,0 +1,37 @@
+var wikiImage = function (data) {
+    var readData = $("<div>" + data.parse.text["*"] + "</div>");
+    // handle redirects
+    var redirect = readData.find('li:contains("REDIRECT") a').text();
+    if (redirect != "") {
+      callWikipediaAPI(redirect);
+      return;
+    }
+
+    var box = readData.find(".infobox"); // right column wikipedia box
+    var imageURL = null;
+    // Check if page has images
+    if (data.parse.images.length >= 1) { //get the one on the box
+      imageURL = box.find("img").first().attr("src");
+    }
+
+    $("#wiki").append('<div><img src="' + imageURL + '"/>');
+  };
+
+  function callWikipediaAPI(wikiPage) {
+    // http://www.mediawiki.org/wiki/API:Parsing_wikitext#parse
+    $.getJSON("http://es.wikipedia.org/w/api.php?action=parse&format=json&callback=?",
+      {
+        page: wikiPage,
+        prop: "text|images",
+      }, wikiImage);
+    $.getJSON("https://es.wikipedia.org/w/api.php?format=json&formatversion=2&action=query&prop=extracts&exintro=&explaintext=",
+      {// new API json format, easier to handle
+        redirect: 1,
+        titles: wikiPage,
+        origin: "*" // required by CORS, API includes allow credentials header
+      }, function(data) {
+        var content = data.query.pages[0].extract;
+        content = content.replace(/^\d+$/," ");
+        $("#wiki").append("<p>" + content + "</p>)");
+      });
+  }
