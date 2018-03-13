b63253c98f8c867083f216ea55b3a29c017c3d18
OriolInvernonLlaneza <93silf@gmail.com>
2018-03-13 22:27:56 +0100

    Probando snowballC, más stopwords

--- R/test.R
+++ R/test.R
@@ -4,7 +4,7 @@ library("SnowballC")
 library("RColorBrewer")
 library("wordcloud")
 
-#setwd("D:/Users/Oriol/Documents/practicas/proyecto/R")
+setwd("D:/Users/Oriol/Documents/practicas/proyecto/R")
 ex  <- VCorpus(DirSource(directory = "cartas\\ejemplo", encoding = "UTF-8"), readerControl = list(language="es"))
 summary(ex)  #check what went in
 
@@ -15,9 +15,9 @@ ex <- tm_map(ex, content_transformer(tolower)) #a minus
 ex <- tm_map(ex, PlainTextDocument)
 ex <- tm_map(ex, removeNumbers) #números
 ex <- tm_map(ex, removePunctuation) #puntuación
-ex <- tm_map(ex, stripWhitespace) #espacios en blanco extras
 ex <- tm_map(ex, removeWords, c(stopwords("spanish"), customStopwords)) #Borrar palabras vacías.
-ex <- tm_map(ex, stemDocument) 
+ex <- tm_map(ex, stripWhitespace) #espacios en blanco extras
+wordStem(ex[[1]], language="spanish")
 
 inspect(ex[[1]])
 
