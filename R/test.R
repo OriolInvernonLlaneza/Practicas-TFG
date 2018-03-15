9dbf32a8184d2c1ecf592f8fe456303c8e6ce1b4
OriolInvernonLlaneza <93silf@gmail.com>
2018-03-15 18:03:35 +0100

    StemCompletion does not work

--- R/test.R
+++ R/test.R
@@ -1,23 +1,30 @@
 library("NLP")
 library("tm") #load text mining library
-library("SnowballC")
 library("RColorBrewer")
-library("wordcloud")
+library("wordcloud2")
+library("RTextTools")
 
 setwd("D:/Users/Oriol/Documents/practicas/proyecto/R")
-ex  <- VCorpus(DirSource(directory = "cartas\\ejemplo", encoding = "UTF-8"), readerControl = list(language="es"))
-summary(ex)  #check what went in
 
 customStopwords <- read.table("stopwordsJovellanos.txt", header = TRUE)
 customStopwords <- as.vector(customStopwords$WORDS)
 
-ex <- tm_map(ex, content_transformer(tolower)) #a minus
-ex <- tm_map(ex, PlainTextDocument)
-ex <- tm_map(ex, removeNumbers) #números
-ex <- tm_map(ex, removePunctuation) #puntuación
-ex <- tm_map(ex, removeWords, c(stopwords("spanish"), customStopwords)) #Borrar palabras vacías.
-ex <- tm_map(ex, stripWhitespace) #espacios en blanco extras
-wordStem(ex[[1]], language="spanish")
+ex  <- VCorpus(DirSource(directory = "cartas\\ejemplo", encoding = "UTF-8"), readerControl = list(language="es"))
+
+cleanCorpus <- function(corpus){
+  corpus <- tm_map(corpus, content_transformer(tolower)) #a minus
+  corpus <- tm_map(corpus, removeNumbers) #números
+  corpus <- tm_map(corpus, removePunctuation) #puntuación
+  corpus <- tm_map(corpus, removeWords, c(stopwords("spanish"), customStopwords)) #Borrar palabras vacías.
+  corpus <- tm_map(corpus, stripWhitespace) #espacios en blanco extras
+  corpus <- tm_map(corpus, PlainTextDocument)  # needs to come before stemming
+  return(corpus)
+}
+
+clean <- cleanCorpus(ex)
+cleanCopy <- clean
+stemmed <- tm_map(clean, stemDocument, "spanish")
+#stemmedC <- tm_map(stemmed, stemCompletion, dictionary=cleanCopy)
 
 inspect(ex[[1]])
 
