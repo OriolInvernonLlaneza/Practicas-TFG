3955bedfa82e72ec8a615e670a770d1ebfb8a840
93silf@gmail.com <93silf@gmail.com>
2018-03-27 12:52:18 +0200

    TopicModels

--- R/test.R
+++ R/test.R
@@ -7,22 +7,24 @@ library("cluster")
 library("fpc")
 library("dbscan")
 library("factoextra")
+library("tidytext")
+library("topicmodels")
 #library("RColorBrewer")
 #library("wordcloud2")
 #library("RTextTools")
 
-setwd("D:/Users/Oriol/Documents/practicas/proyecto/R")
-#setwd("C:/Users/Becario-2/Desktop/RepoOri1718/practicas/Practicas-TFG/R")
+#setwd("D:/Users/Oriol/Documents/practicas/proyecto/R")
+setwd("C:/Users/Becario-2/Desktop/RepoOri1718/practicas/Practicas-TFG/R")
 
 customStopwords <- read.table("stopwordsJovellanos.txt", header = TRUE)
 customStopwords <- as.vector(customStopwords$WORDS)
 
 #ex  <- VCorpus(DirSource(directory = "cartas\\ejemplo", encoding = "UTF-8"), readerControl = list(language="es"))
 csv <- read.csv("cartas\\CorrespondenciaJove.csv", sep =";", header = TRUE, encoding = "UTF-8")
-ex <- VCorpus(VectorSource(csv$TextoCarta[203:203]))
+ex <- VCorpus(VectorSource(csv$TextoCarta[1:205]))
 
 cleanCorpus <- function(corpus){
-  #corpus <- tm_map(corpus, content_transformer(tolower)) #a minus
+  corpus <- tm_map(corpus, content_transformer(tolower)) #a minus
   corpus <- tm_map(corpus, removeNumbers) #numbers
   corpus <- tm_map(corpus, removePunctuation) #punt
   corpus <- tm_map(corpus, content_transformer(function(n) { n <- gsub("[¡¿'«»]", "", n)}))
@@ -132,3 +134,35 @@ db <- dbscan(as.matrix(tfxidf), 1.4, 3)
 fviz_cluster(db, data = as.matrix(tfxidf), stand = FALSE,
              ellipse = FALSE, show.clust.cent = FALSE,
              geom = "point",palette = "jco", ggtheme = theme_classic())
+
+#Topic modeling
+#Set parameters for Gibbs sampling
+burnin <- 4000
+iter <- 1500
+thin <- 500
+seed <-list(2003,5,63,100001,765)
+nstart <- 5
+best <- TRUE
+#Number of topics
+k <- 15
+ldaOut <-LDA(dtm, k, method="Gibbs", control=list(nstart=nstart, seed = seed, best=best, burnin = burnin, iter = iter, thin=thin))
+#write out results
+#docs to topics
+ldaOut.topics <- as.matrix(topics(ldaOut))
+write.csv(ldaOut.topics,file=paste("TopicModel/LDAGibbs",k,"DocsToTopics.csv"))
+#top 10 terms in each topic
+ldaOut.terms <- as.matrix(terms(ldaOut,10))
+write.csv(ldaOut.terms,file=paste("TopicModel/LDAGibbs",k,"TopicsToTerms.csv"))
+#probabilities associated with each topic assignment
+topicProbabilities <- as.data.frame(ldaOut@gamma)
+write.csv(topicProbabilities,file=paste("TopicModel/LDAGibbs",k,"TopicProbabilities.csv"))
+#Find relative importance of top 2 topics
+topic1ToTopic2 <- lapply(1:nrow(dtm),function(x){
+      sort(topicProbabilities[x,])[k]/sort(topicProbabilities[x,])[k-1]})
+#Find relative importance of second and third most important topics
+topic2ToTopic3 <- lapply(1:nrow(dtm),function(x) {
+        sort(topicProbabilities[x,])[k-1]/sort(topicProbabilities[x,])[k-2]})
+#write to file
+write.csv(topic1ToTopic2,file=paste("TopicModel/LDAGibbs",k,"Topic1ToTopic2.csv"))
+write.csv(topic2ToTopic3,file=paste("TopicModel/LDAGibbs",k,"Topic2ToTopic3.csv"))
+
