16af90ba98a01b32ae57ee577aa3ea42cd344055
OriolInvernonLlaneza <93silf@gmail.com>
2018-03-27 14:42:40 +0200

    cross validation TopicModels

--- R/test.R
+++ R/test.R
@@ -9,12 +9,15 @@ library("dbscan")
 library("factoextra")
 library("tidytext")
 library("topicmodels")
+library("doParallel")
+library("ggplot2")
+library("scales")
 #library("RColorBrewer")
 #library("wordcloud2")
 #library("RTextTools")
 
-#setwd("D:/Users/Oriol/Documents/practicas/proyecto/R")
-setwd("C:/Users/Becario-2/Desktop/RepoOri1718/practicas/Practicas-TFG/R")
+setwd("D:/Users/Oriol/Documents/practicas/proyecto/R")
+#setwd("C:/Users/Becario-2/Desktop/RepoOri1718/practicas/Practicas-TFG/R")
 
 customStopwords <- read.table("stopwordsJovellanos.txt", header = TRUE)
 customStopwords <- as.vector(customStopwords$WORDS)
@@ -135,10 +138,60 @@ fviz_cluster(db, data = as.matrix(tfxidf), stand = FALSE,
              ellipse = FALSE, show.clust.cent = FALSE,
              geom = "point",palette = "jco", ggtheme = theme_classic())
 
-#Topic modeling
-#Set parameters for Gibbs sampling
+#----------------5-fold cross-validation, different numbers of topics----------------
+# set up a cluster for parallel processing
+cluster <- makeCluster(detectCores(logical = TRUE) - 1) # leave one CPU spare...
+registerDoParallel(cluster)
+
+# load up the needed R package on all the parallel sessions
+clusterEvalQ(cluster, {
+  library(topicmodels)
+})
+
+full_data <- dtm
+n <- nrow(full_data)
 burnin <- 4000
 iter <- 1500
+keep <- 50
+folds <- 5
+splitfolds <- sample(1:folds, n, replace = TRUE)
+candidate_k <- c(3, 5, 7, 10, 15, 20, 25, 30, 40, 50, 75, 100, 200, 300) # candidates for how many topics
+
+# export all the needed R objects to the parallel sessions
+clusterExport(cluster, c("full_data", "burnin", "iter", "keep", "splitfolds", "folds", "candidate_k"))
+
+# we parallelize by the different number of topics.  A processor is allocated a value
+# of k, and does the cross-validation serially.  This is because it is assumed there
+# are more candidate values of k than there are cross-validation folds, hence it
+# will be more efficient to parallelise
+system.time({
+  results <- foreach(j = 1:length(candidate_k), .combine = rbind) %dopar%{
+    k <- candidate_k[j]
+    results_1k <- matrix(0, nrow = folds, ncol = 2)
+    colnames(results_1k) <- c("k", "perplexity")
+    for(i in 1:folds){
+      train_set <- full_data[splitfolds != i , ]
+      valid_set <- full_data[splitfolds == i, ]
+      
+      fitted <- LDA(train_set, k = k, method = "Gibbs",
+                    control = list(burnin = burnin, iter = iter, keep = keep) )
+      results_1k[i,] <- c(k, perplexity(fitted, newdata = valid_set))
+    }
+    return(results_1k)
+  }
+})
+stopCluster(cluster)
+
+results_df <- as.data.frame(results)
+
+ggplot(results_df, aes(x = k, y = perplexity)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  ggtitle("5-fold cross-validation of topic modelling with the 'CartasJove' dataset") +
+  labs(x = "Candidate number of topics", y = "Perplexity when fitting the trained model to the hold-out set")
+
+#Topic modeling
+#Set parameters for Gibbs sampling
 thin <- 500
 seed <-list(2003,5,63,100001,765)
 nstart <- 5
